d754b0d2a7a84751fb8c8e0c17dda19d225e754f
Tony Bruno <42123553+OceanSBlueTyphoon21@users.noreply.github.com>
2022-03-14 13:40:07 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
-
-
 /**
- * Design Project 4 - Stepper Motor & I2C
+ * Design Project 4 - Stepper Motor & I2C (part 1)
  * Author: Anthony Bruno
  *
- * Description: .....
+ * Description: This program is meant to establish a working I2C serial transmission
+ *              to an Adafruit Featherwing Motor Driver. This goal of this program
+ *              is to collect I2C SDA and SCL samples with a logic analyzer. The
+ *              Sampling rate is set for 1MHz and the number of samples taken is
+ *              20M Samples. The I2C is set for a data rate of 400Kbps (Fast rate).
+ *
+ * STARTUP FILE EDITS:
+ * - Line 120:      LDR   r10, = main
+ * - Line 165:LDR   pc, [pc,#-8]
+ *
  */
 
-// Define for register-value access
-
+// Libraries and imports
 #include <stdio.h>
 
+// Register Access Macro
 #define HWREG(x) (*((volatile unsigned int *)(x)))
 
 // module CLKWKUPS
@@ -24,7 +31,6 @@
 
 // Global Variables & Stacks
 int x;
-
 volatile unsigned int USR_STACK[100];
 volatile unsigned int IRQ_STACK[100];
 
@@ -43,53 +49,35 @@ int main(void)
     asm("ADD R13, R13, #0x100");
     asm("CPS #0x13");
 
-    // Initialize the I2C1 Module
-    I2C_INIT();
-    I2CTransmit();
+    I2C_INIT();         // Initialize the I2C1 Module
+    I2CTransmit();      // Transmit data to Featherwing board
 
-
-	return 0;
+    return 0;           // Terminate Program
 }
 
-// ----------------  Function definitions  -------------------
+// __________________________ FUNCTIONS _____________________________________
 
 void I2CTransmit(void)
 {
     while((HWREG(I2C1BA + 0x24) & 0x1000) != 0x0){}   // Polling Bit 12 (BB) in I2C_IRQSTATUS_RAW
-    // Setup the number of Data bytes per transmission
-    HWREG(I2C1BA + 0x98) = 0x1;
-    HWREG(I2C1BA + 0xA4) = 0x8603;      // Initiate a START/STOP condition
+    HWREG(I2C1BA + 0x98) = 0x2;         // Setup the number of Data Bytes
+    HWREG(I2C1BA + 0xA4) = 0x8603;      // Initiate a START/STOP condition, setup I2C as master & Transmitter
 
-    while((HWREG(I2C1BA + 0x24) & 0x10) != 0x10){} // Polling the XRDY bit
-    HWREG(I2C1BA + 0x2C) = 0x10;    // I2C_IRQENABLE_SET
-    HWREG(I2C1BA + 0x9C) = 0xA9;    // Data to Send
+    HWREG(I2C1BA + 0x2C) = 0x10;    // Set XRDY_IE bit in I2C_IRQENABLE_SET
+    HWREG(I2C1BA + 0x9C) = 0x00;    // PCA9685 Control Register (MODE1 Register)
+    HWREG(I2C1BA + 0x2C) = 0x10;    // Set XRDY_IE bit in I2C_IRQENABLE_SET
+    HWREG(I2C1BA + 0x9C) = 0x11;    // Data to MODE1 register
 }
 
 void I2C_INIT(void)
 {
     HWREG(ControlBA + 0x95C) = 0x32;    // set pin 17 to I2C_SCL Mode
     HWREG(ControlBA + 0x958) = 0x32;    // set pin 18 to I2C_SDA Mode
-
-    // turn on the I2C1 module with Clock
-    HWREG(CMPERWKUP + 0x48) = 0x2;
-
-    // SW reset of the I2C1 Module
-    //HWREG(I2C1BA + 0x10) = 0x2;
-
-    // program the Prescalar register to get 12MHz for Clock to I2C1 module
-    HWREG(I2C1BA + 0xB0) = 0x3;
-
-    // Setup the I2C_SCL to obtain 400Kbps (using I2C_SCLL & _SCLH)
-    HWREG(I2C1BA + 0xB4) = 0x8;      // setup of I2C_SCLL
-    HWREG(I2C1BA + 0xB8) = 0xA;      // setup of I2C_SCLH
-
-    // Setup the I2C Own Address (of the master)
-    HWREG(I2C1BA + 0xA8) = 0x0;
-
-    // Take the I2C1 out of reset, configure as master mode and transmitter mode
-    HWREG(I2C1BA + 0xA4) = 0x8000;
-
-    // Setup the slave address
-    HWREG(I2C1BA + 0xAC) = 0x65;
+    HWREG(CMPERWKUP + 0x48) = 0x2;      // Turn on I2C1 Clock Module
+    HWREG(I2C1BA + 0xB0) = 0x3;         // Set Prescalar register to 12MHz
+    HWREG(I2C1BA + 0xB4) = 0x8;         // setup of I2C_SCLL for 400Kbps
+    HWREG(I2C1BA + 0xB8) = 0xA;         // setup of I2C_SCLH for 400Kbps
+    HWREG(I2C1BA + 0xA8) = 0x0;         // setup I2C1 Own Address (Master)
+    HWREG(I2C1BA + 0xA4) = 0x8000;      // Take the I2C1 out of reset
+    HWREG(I2C1BA + 0xAC) = 0x60;        // Setup SLAVE Address
 }
-
